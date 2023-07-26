d6116e73ff28e3f8f9db1e83f313a73eae28f4a5
Sluveek <chirita.veaceslav92@gmail.com>
2023-07-27 00:11:12 +0200

    Create views and URLs

--- Furniture_store/store/views.py
+++ Furniture_store/store/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    comments = {}
+    return render(request, "index.html", comments)
+
+
+def cart(request):
+    comments = {}
+    return render(request, "cart.html", comments)
