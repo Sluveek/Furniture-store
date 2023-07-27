23522c154920aa6da9f7e6cf577f71ae4d49fd70
Veaceslav Veaceslav <chirita.veaceslav92@gmail.com>
2023-07-27 18:54:47 +0000

    Update templates

--- store/models.py
+++ store/models.py
@@ -8,10 +8,10 @@ class Product(models.Model):
     price = models.IntegerField()
     picture = models.ImageField(upload_to="img", default="")
     
-    
     def __str__(self):
         return self.name
     
+
 class Cart(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True)
     user = models.ForeignKey(User,  on_delete=models.CASCADE)
@@ -19,6 +19,18 @@ class Cart(models.Model):
     
     def __str__(self):
         return str(self.id)
+    
+    @property
+    def total_price(self):
+        cartitems = self.cartitems.all()
+        total = sum([item.price for item in cartitems])
+        return total
+    
+    @property
+    def num_of_items(self):
+        cartitems = self.cartitems.all()
+        quantity = sum([item.quantity for item in cartitems])
+        return quantity
     
 
 class CartItem(models.Model):
@@ -28,3 +40,8 @@ class CartItem(models.Model):
     
     def __str__(self):
         return self.product.name
+    
+    @property
+    def price(self):
+        new_price = self.product.price * self.quantity
+        return new_price
--- store/urls.py
+++ store/urls.py
@@ -4,4 +4,5 @@ from .import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("cart", views.cart, name="cart"),
+    path("add_to_cart", views.add_to_cart, name= "add")
 ]
