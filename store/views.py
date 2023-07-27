884b441f450cfd4f2db1d4270595685f8f82e8a5
Veaceslav Veaceslav <chirita.veaceslav92@gmail.com>
2023-07-27 09:49:37 +0000

    Create Views and URLs

--- store/views.py
+++ store/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    comments = {}
+    return render(request, "index.html", comments)
+
+
+def cart(request):
+    comments = {}
+    return render(request, "cart.html", comments)
